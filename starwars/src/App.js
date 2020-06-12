0680189bebbca1e50a493e51490446146bcce1e7
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-06-12 10:03:36 -0700

    Styles are mostly complete, adding compatability for mobile and desktop

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import syled from 'styled-components';
+import styled from 'styled-components';
 import './App.css';
 import CharacterCard from './components/Character';
 
@@ -14,27 +14,49 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-useEffect(() =>{
-  axios.get('https://swapi.dev/api/people/')
-  .then(response =>{
-    console.log(response.data.results)
-    setCharacters(response.data.results)
-    // debugger
-  })
-  .catch(error =>{
-    console.log(error)
-  })
-}, []);
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people/')
+    .then(response =>{
+      console.log(response.data.results)
+      setCharacters(response.data.results)
+      // debugger
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+  }, []);
 
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-      {characters.map((item) =>{
-        return <CharacterCard character = {item}/>
-      })}
-    </div>
+    <AppDiv>
+      <h1>Characters</h1>
+      <div>
+        {characters.map((item) =>{
+          return <CharacterCard key ={item.name} character = {item}/>
+        })}
+      </div>
+    </AppDiv>
   );
 };
 
 export default App;
+
+const AppDiv = styled.div`
+  h1{
+    text-align: center;
+    background-color: white;
+    opacity: 60%;
+    padding: 5%;
+    font-size: 3rem;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  }
+
+  div{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+  }
+  
+`
