a87f963496de78eda798a0649e14d0e0cca91294
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-06-12 09:08:45 -0700

    Character component started, data recieved

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+import syled from 'styled-components';
+
+const charactersCard = (props) =>{
+
+    const {character} = props;
+
+    return(
+        <div>
+            <p>{character}</p>
+        </div>
+    );
+};
+export default charactersCard;
